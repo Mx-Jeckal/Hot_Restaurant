355c98e848f4066807d5832cdac017d634496e82
Lawrence Adrian De Belen <ltdebelen@gmail.com>
2020-02-22 12:56:51 -0600

    Fixing routes in index

--- server.js
+++ server.js
@@ -4,7 +4,6 @@ var express = require("express");
 
 var path = require("path");
 
-
 var Reservation = require("./lib/reservation");
 
 // Sets up the Express App
@@ -16,54 +15,57 @@ app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 // Reservations (DATA)
 // =============================================================
-var reservations = []
+var reservations = [];
 
-var waitList = []
-
+var waitList = [];
 
 // Pages routes -----------------------------------
 
 app.get("/", function(req, res) {
-    res.sendFile(path.join(__dirname, "index.html"));
+  res.sendFile(path.join(__dirname, "index.html"));
+});
+
+app.get("/table", function(req, res) {
+  res.sendFile(path.join(__dirname, "tables.html"));
 });
 
 app.get("/add", function(req, res) {
-    res.sendFile(path.join(__dirname, "reservations.html"));
+  res.sendFile(path.join(__dirname, "reservations.html"));
 });
 
 // API routes -----------------------------
 
 // Displays all characters
 app.get("/api/reservations", function(req, res) {
-    return res.json(reservations);
+  return res.json(reservations);
 });
 
 app.get("/api/waitList", function(req, res) {
-    return res.json(waitList);
+  return res.json(waitList);
 });
 
 // Displays a single character, or shows "No character found"
 app.get("/api/reservations/:id", function(req, res) {
-    // Grab the selected parameter
-    var id = req.params.id;
+  // Grab the selected parameter
+  var id = req.params.id;
 
-    // Filter to show only the selected character
-    for (var i = 0; i < reservations.length; i++) {
-        if (id === reservations[i].id) {
-            return res.json(reservations[i]);
-        }
+  // Filter to show only the selected character
+  for (var i = 0; i < reservations.length; i++) {
+    if (id === reservations[i].id) {
+      return res.json(reservations[i]);
     }
+  }
 
-    // Otherwise display "No character found"
-    return res.send("No person found!");
+  // Otherwise display "No character found"
+  return res.send("No person found!");
 });
 app.post("/api/reservations", function(req, res) {
-    reservations.push(req.body)
-    res.json(reservations)
-})
+  reservations.push(req.body);
+  res.json(reservations);
+});
 
 // Listener
 // ===========================================================
 app.listen(PORT, function() {
-    console.log("App listening on PORT http://localhost:" + PORT);
-})
+  console.log("App listening on PORT http://localhost:" + PORT);
+});
